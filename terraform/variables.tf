21a02c1267fa5d5042b590add932b0d155497d3d
Alexey Eshenko <alexeyeschenko@gmail.com>
2020-02-27 13:21:58 +0300

    HW-07 Add terraform env

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,18 +3,5 @@ variable project {
 }
 variable region {
   description = "Region"
-  default     = "europe-north1-a"
-}
-variable public_key_path {
-  description = "Path to the public key used for ssh access"
-}
-variable disk_image {
-  description = "Disk image"
-}
-variable private_key_path {
-  description = "Path to the private key used for ssh access"
-}
-variable zone {
-  description = "Zone"
-  default     = "europe-north1-a"
+  default     = "europe-north1"
 }
